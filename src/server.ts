765f4f15637753473dd75b2084d0211d88798a1f
Razvan Onofrei <razwan.me@gmail.com>
2022-10-12 12:26:39 +0300

    Remove cors package

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import { readCSV } from './csv';
 import { getMoviesFromRows } from './movie';
-import cors from 'cors';
 import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('dist'));
-app.options( '/movies', cors() );
 
 app.get( '/', async (req, res) => {
     res.sendFile(path.resolve(__dirname, '../index.html'));
 } );
 
-app.get( '/movies', cors(), async (req, res) => {
+app.get( '/movies', async (req, res) => {
     const rows = await readCSV("./src/movies.csv");
     const movies = getMoviesFromRows( rows ); 
 
